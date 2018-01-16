32bb99334532b4bc906d35131b331f1b365cbf74
RobJMal <robjmal@gmail.com>
2018-01-16 14:58:09 -0500

    Added SMS and email capabilities

--- Api-starter-app-master/routes/routes.js
+++ Api-starter-app-master/routes/routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const auth = require('../controllers/auth')
 const users = require('../controllers/users')
-
+const send = require('../controllers/send')
 
 /*
 * User Routes
@@ -16,11 +16,27 @@ router.route('/users')
   .put(auth.validateUser, users.updateUser)
   .delete(auth.validateUser, users.deleteUser)
 
+
+
+
 /*
 * Auth Routes
 */
 router.route('/auth/login')
   .post(auth.loginUser);
+
+
+/*
+* Send Routes
+*/
+// Sends user text message of result 
+router.route('/results/sendText')
+	.post(send.sendUserTextMessage)
+
+// Sends user email of result 
+router.route('/results/sendEmail')
+	.post(send.sendUserEmail)
+
 
 // expose routes through router object
 module.exports = router;
