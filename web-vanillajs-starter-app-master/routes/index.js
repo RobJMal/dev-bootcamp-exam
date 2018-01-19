4817a9489d90fc7dc6079bbbf304a05666015c75
RobJMal <robjmal@gmail.com>
2018-01-19 10:14:42 -0500

    Fixing merge conflicts for testing

--- web-vanillajs-starter-app-master/routes/index.js
+++ web-vanillajs-starter-app-master/routes/index.js
@@ -7,11 +7,6 @@ router.get('/', (req, res, next) => {
     return res.render('index');
 });
 
-<<<<<<< HEAD
-router.get('/welcome', (req, res, next) => {
-	return res.render('welcome')
-})
-
 router.get('/makeQuestion', (req, res, next) => {
 	return res.render('makeQuestion')
 })
@@ -27,7 +22,6 @@ router.get('/question', (req, res, next) => {
 	return res.render('question')
 })
 
-=======
 router.get('/results', (req, res, next) => {
     return res.render('results');
 });
@@ -59,15 +53,6 @@ router.post('/myresults/phone', (req, res, next) => {
   }).pipe(res)
 })
 
-
-//so this is unused now...
->>>>>>> 47fc74eed8724809516857b29be73ab016b6d61a
-router.post('/register', (req, res, next) => {
-  request.post({
-      url: config.apiUrl + '/users',
-      form: req.body
-  }).pipe(res)
-})
 
 router.post('/login', (req, res, next) => {
   request.post({
